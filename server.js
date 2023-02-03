565f4d98a27b3f8807c3c7093654302aef507084
Peter michoma <pmichoma@kabarak.ac.ke>
2023-02-03 06:47:12 -0500

    setting up the modules

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -9,6 +11,16 @@ const PORT = process.env.PORT || 3001
 
 app.use(morgan('tiny'));
 
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.set("view engine" ,"ejs");
+
+app.use('/css' , express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img' , express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js' , express.static(path.resolve(__dirname, "assets/js")))
+
+app.set("view engine" , "ejs")
+app.set("views",)
 app.get('/' , (req,res)=>{
     res.send("these is a crud application");
 })
